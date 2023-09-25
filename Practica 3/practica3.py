cd192507a3db1b3eb8850374be3855524fe9a5ab
aaron <aaronciempo@gmail.com>
2023-09-25 13:54:35 +0200

    refreshPract3

--- Practica 3/practica3.py
+++ Practica 3/practica3.py
@@ -6,10 +6,30 @@ print("Content-type: text/html\n")
 numero = 5
 texto = "5"
 
-print(numero)
-print(type(numero))
+print(numero) #Imprime el valor de la variable
+print(type(numero)) #Imprime el tipo de variable 
 
 print(texto)
 print(type(texto))
+
+x, y, z = "Orange", "Banana", "Cherry"  
+#Asignar valor a varias variables a la vez. 
+#El numero de variables a de ser igual al de asiganciones
+#De lo contrario no asingnara ningun valor
+print(x)
+print(y)
+print(z)
+
+a = b = c = "Orange"  
+#Asignara el mismo valor a todas las variables
+print(a)
+print(b)
+print(c)
+
+fruits = ["manzana","platano","pera"]
+d,e,f = fruits
+print(d)
+print(e)
+print(f)
 
 numero=str(5)
